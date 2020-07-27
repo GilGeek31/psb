91a555827e8034b4ef8f80ff1ecb00523572d613
Edi Siswanto <edisiswanto.ti8.poliwangi@gmail.com>
2020-07-27 21:12:45 +0700

    design landing page

---
+++ index.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>PPDB ONLINE</title>
+  </head>
+  <body>
+    
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-white pt-3 pb-3 shadow ">
+            <div class="container">
+                <a class="navbar-brand font-weight-bold text-dark" href="#">PPDB ONLINE</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="menu-header navbar-nav ml-auto">
+                        <a class="nav-item nav-link active" href="#">TENTANG KAMI</a>
+                        <a class="nav-item nav-link" href="#">STATISTIK</a>
+                        <a class="nav-item nav-link" href="#">JADWAL</a>
+                        <a class="nav-item nav-link" href="#">TESTIMONI</a>
+                        <a class="nav-item nav-link" href="#">MASUK</a>
+                        <a class="btn btn-danger rounded-pill" href="#">DAFTAR</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h3 class="mb-3">
+                        PPDB ONLINE <br />
+                        SMK NEGERI DARUL ULUM MUNCAR
+                    </h3>
+                    <p class="mb-5">Untuk calon Pendaftar tahun ajaran 2020/2021 bisa mendaftar melalui website ini atau langsung datang ke tempat pendaftaran</p>
+
+                    <a href="" class="btn btn-danger rounded-pill">DAFTAR SEKARANG</a>
+                </div>
+                <div class="col-md-6">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="text-center mb-5">
+                <h3 class="mb-3">KENAPA PILIH <br /> SMKN DARUL ULUM ?</h3>
+                <p>Beberapa alasan kalian harus bergabung dengan SMKN Darul Ulum dan bersiap menggapai masa depan gemilang.</p>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="fasilitas">
+                        <div class="description">
+                            <h5>Fasilitas Lengkap</h5>
+                            <p>Fasilitas penunjang KBM yang lengkap dan berkualitas premium akan memudahkan kalian dalam belajar</p>
+                        </div>
+                        <div class="icon-des">
+                            <i class="fa fa-home"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
