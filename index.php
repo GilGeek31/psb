28364ebf9078c69bd685fec4d6b629c993465b78
Edi Siswanto <edisiswanto.ti8.poliwangi@gmail.com>
2020-07-28 21:59:02 +0700

    Landing page Section alasan, tentang untuk part 2

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <h3 class="mb-3">
+                    <h3 class="h1 font-weight-bold mt-3 mb-3">
                         PPDB ONLINE <br />
                         SMK NEGERI DARUL ULUM MUNCAR
                     </h3>
@@ -48,7 +48,7 @@
                     <a href="" class="btn btn-danger rounded-pill">DAFTAR SEKARANG</a>
                 </div>
                 <div class="col-md-6">
-                    <img src="" alt="">
+                    <img src="assets/img/image-hero.svg" alt="gambar hero" class="img-fluid d-block mx-auto" style="max-height: 300px">
                 </div>
             </div>
         </div>
@@ -57,20 +57,115 @@
     <section>
         <div class="container">
             <div class="text-center mb-5">
-                <h3 class="mb-3">KENAPA PILIH <br /> SMKN DARUL ULUM ?</h3>
+                <h3 class="mb-3 font-weight-bold">KENAPA PILIH <br /> SMKN DARUL ULUM ?</h3>
                 <p>Beberapa alasan kalian harus bergabung dengan SMKN Darul Ulum dan bersiap menggapai masa depan gemilang.</p>
             </div>
             <div class="row">
+                <div class="col-md-6">
+                    <div class="fasilitas active">
+                        <div class="description">
+                            <h5>Fasilitas Lengkap</h5>
+                            <p>Fasilitas penunjang KBM yang lengkap dan berkualitas premium akan memudahkan kalian dalam belajar</p>
+                        </div>
+                        <div class="icon-des text-center">
+                            <img src="assets/img/icons/book.svg" alt="buku" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="fasilitas">
+                        <div class="description icon-left">
+                            <h5>Fasilitas Lengkap</h5>
+                            <p>Fasilitas penunjang KBM yang lengkap dan berkualitas premium akan memudahkan kalian dalam belajar</p>
+                        </div>
+                        <div class="icon-des text-center">
+                            <img src="assets/img/icons/building.svg" alt="buku" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
                 <div class="col-md-6">
                     <div class="fasilitas">
                         <div class="description">
                             <h5>Fasilitas Lengkap</h5>
                             <p>Fasilitas penunjang KBM yang lengkap dan berkualitas premium akan memudahkan kalian dalam belajar</p>
                         </div>
-                        <div class="icon-des">
-                            <i class="fa fa-home"></i>
+                        <div class="icon-des text-center">
+                            <img src="assets/img/icons/check.svg" alt="buku" class="img-fluid">
                         </div>
                     </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="fasilitas">
+                        <div class="description icon-left">
+                            <h5>Fasilitas Lengkap</h5>
+                            <p>Fasilitas penunjang KBM yang lengkap dan berkualitas premium akan memudahkan kalian dalam belajar</p>
+                        </div>
+                        <div class="icon-des text-center">
+                            <img src="assets/img/icons/checklist.svg" alt="buku" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="fasilitas">
+                        <div class="description">
+                            <h5>Fasilitas Lengkap</h5>
+                            <p>Fasilitas penunjang KBM yang lengkap dan berkualitas premium akan memudahkan kalian dalam belajar</p>
+                        </div>
+                        <div class="icon-des text-center">
+                            <img src="assets/img/icons/people.svg" alt="buku" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="fasilitas">
+                        <div class="description icon-left">
+                            <h5>Fasilitas Lengkap</h5>
+                            <p>Fasilitas penunjang KBM yang lengkap dan berkualitas premium akan memudahkan kalian dalam belajar</p>
+                        </div>
+                        <div class="icon-des text-center">
+                            <img src="assets/img/icons/proyektor.svg" alt="buku" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="tentang-kami">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="assets/img/tentang-sekolah.svg" alt="gambar sekolah" class="img-fluid mx-auto d-block">
+                </div>
+                <div class="col-md-6">
+                    <h3 class="mt-5 mb-3 font-weight-bold">TENTANG SMKNDU</h3>
+                    <p class="description mb-5">
+                        SMKNDU Adalah Sekolah menengah kejuruan negeri yang berdiri sejak 2003. SMKNDU Adalah Sekolah menengah kejuruan negeri yang berdiri sejak 2003. SMKNDU Adalah Sekolah menengah kejuruan negeri yang berdiri sejak 2003. SMKNDU Adalah Sekolah menengah kejuruan negeri yang berdiri sejak 2003. 
+                    </p>
+
+                    <p class="font-weight-bold">Kontak Kami:</p>
+                    <ul class="social">
+                        <li>
+                            <a href="">
+                                <img src="assets/img/icons/telephone.svg" alt="telephone" class="img-fluid">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src="assets/img/icons/email.svg" alt="email" class="img-fluid">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src="assets/img/icons/internet.svg" alt="internet" class="img-fluid">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src="assets/img/icons/facebook.svg" alt="facebook" class="img-fluid">
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
